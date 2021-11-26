c8e728ff487c9df6460822140f3685ab0196ba61
JorgeDeLara <mentorlinksr12@outlook.com>
2021-11-26 17:21:48 -0600

    Adding T2

---
+++ 01_OOP/T2/DN_1.2_Jorge_DeLara_v1.0/AudioManager/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; 
+//This is the name of the class library project
+namespace AudioManager
+{
+    //This is the name of the library class and is the base too
+    public class Vehicle
+    {
+        //This is a metod/function called VehicleSound
+        //The next part we'll add sounds for some vehicles
+        public void VehicleSound()
+        {
+            //Here is all the code for each son class.
+            Console.WriteLine("I'm a vehicle, and soon I'll have sounds");
+        }
+    }
+}
---
+++ 01_OOP/T2/DN_1.2_Jorge_DeLara_v1.0/DN_1.2_Jorge_DeLara_v1.0/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//We use this to work with Vehicle, is necessary add a service preference
+using AudioManager;
+//This es the workspace
+namespace DN_1._1_Jorge_DeLara_v1._0
+{
+    //This is the name of the base principal class
+    internal class Program
+    {   
+        //This is the structure of the main function his type is void.
+        static void Main(string[] args)
+        {
+            //These instruccions works to display menssages to user in the console
+            Console.Write("Hi I'm a program that shows the functionality of Vehicle class\n");
+            Console.WriteLine("I'll add the sounds of diferent vehicles the next time");
+            
+            //We create an object from vehicle.cs
+            Vehicle vehicle=new Vehicle();
+            //We use one function from Vehicle
+            vehicle.VehicleSound();
+            //This code has the function of wait one key from the keyboard to finish
+            Console.ReadKey();
+
+        }
+    }
+}
