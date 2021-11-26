f132a3e1bab8ea0533d88070f857c615affa7de9
JorgeDeLara <mentorlinksr12@outlook.com>
2021-11-25 21:03:58 -0600

    Ejemplo Video 1 y 2 en el mismo proyecto.

---
+++ 01_OOP/E1/OOP.ConsoleExample/AudioManager/Animal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioManager
+{//We need public because we have to use these classes
+//in all the principal program.
+    public class Animal
+    {
+        //We need virtual to modify the content of father to sons//
+        public  virtual void AnimalSound()
+        {
+            Console.WriteLine("I am an Animal");
+        }
+
+    }
+}
---
+++ 01_OOP/E1/OOP.ConsoleExample/OOP.ConsoleExample/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AudioManager;//NUGet
+
+namespace OOP.ConsoleExample
+{
+    internal class Program
+    {
+        //Principal function
+        static void Main(string[] args)
+        {
+            //While= true repeats the cicle
+            while (true)
+            {
+                //We show text in the console
+                Console.WriteLine("Type the animal: ");
+                //Create a variable type String to save keyboard users STring
+                String animalType = Console.ReadLine().ToLower();
+                Animal animal = null;
+                //Switch cicle
+                switch (animalType)
+                {
+                    case "cat":
+                        animal = new Cat();
+                        break;
+                    case "cow":
+                        animal = new Cow();
+                        break;
+                    case "dog":
+                        animal = new Dog();
+                        break;
+                    case "elephant":
+                        animal = new Elephant();
+                        break;
+                    case "lion":
+                        animal = new Lion();
+                        break;
+                    case "pig":
+                        animal = new Pig();
+                        break;
+                    default:
+                        Console.WriteLine("Animal not found");
+                        System.Threading.Thread.Sleep(2000);
+                        break;
+                }
+                
+                if (animal != null)
+                {
+                    animal.AnimalSound();
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
