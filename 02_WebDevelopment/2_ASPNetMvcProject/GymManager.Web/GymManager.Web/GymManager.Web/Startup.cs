c9a77a69f928f9794d8a7bb8ffdbf49d61a726bd
JorgeDeLara <mentorlinksr12@outlook.com>
2021-11-30 19:35:24 -0600

    ASPNetMvcProject Terminado

---
+++ 02_WebDevelopment/2_ASPNetMvcProject/GymManager.Web/GymManager.Web/GymManager.Web/Startup.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GymManager.Web
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseRouting();
+
+            app.UseStaticFiles();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World");
+                });
+                endpoints.MapGet("/File1.html", async context =>
+                {
+                    await context.Response.WriteAsync(DateTime.Now.ToString());
+                });
+            });
+        }
+    }
+}
